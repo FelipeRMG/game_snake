30f523b971f3dc28b87426e133c1a2432f527d9a
FelipeRMG <felipe.r.m.guterres@gmail.com>
2020-08-23 19:52:52 -0300

    Criado a colisão da cobra com a maçã

--- snake.py
+++ snake.py
@@ -1,6 +1,23 @@
 import pygame, random
 from pygame.locals import *
 
+'''
+ TODO
+ 1 - Realizar a colisão da cobra nela mesma e com borda
+ 2 - Bloquear movimento no sentido inverso
+ 3 - criar sistema pontuação
+ 4 - Fazer loop para renascer
+ 5 - Fechar a screen com o botão esc
+'''
+#Função para alinhar a posição da maçã, não precisaria
+def on_grid_random():
+    x = random.randint(0,590)
+    y = random.randint(0,590)
+    return (x // 10 * 10, y // 10 * 10)
+
+def collision(c1, c2):
+    return ((c1[0] == c2[0]) and (c1[1] == c2[1]))
+
 # Direções
 UP = 0
 RIGHT = 1
@@ -17,7 +34,7 @@ snake_skin.fill((0,250,0))# define a cor
 
 apple_skin = pygame.Surface((10,10))
 apple_skin.fill((255,0,0))
-apple_pos = (random.randint(0,590), random.randint(0,590))
+apple_pos = on_grid_random()
 
 my_direction = LEFT
 
@@ -42,6 +59,14 @@ while True:
             if event.key == K_LEFT:
                 my_direction = LEFT
 
+    # decteca a colisão da cobra  com a maçã
+    if collision(snake[0], apple_pos):
+        apple_pos = on_grid_random()
+        snake.append((0,0))
+    # Atualiza o corpo da cobra
+    for i in range(len(snake) - 1, 0, -1):
+        snake [i] = (snake[i-1][0], snake[i-1][1]) # recebe a posição anterior
+
     # Movimentação
     if my_direction == UP: # deslocamento para cima
         snake[0] = (snake[0][0],snake[0][1] - 10)
@@ -52,10 +77,7 @@ while True:
     if my_direction == RIGHT: # deslocamento para cima
         snake[0] = (snake[0][0] + 10,snake[0][1])
 
-    # Atualiza o corpo da cobra
-    for i in range(len(snake) - 1, 0, -1):
-        snake [i] = (snake[i-1][0], snake[i-1][1]) # recebe a posição anterior
-
+    # A
     screen.fill((0,0,0))
     screen.blit(apple_skin,apple_pos)
     for snake_pos in snake:
